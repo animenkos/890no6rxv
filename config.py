789fc94f10e2e5df188ff5e9ee75a45810f94039
animenkos <denjicurse@gmail.com>
2025-09-25 20:35:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # Your telegram bot token
-DEVELOPER_ID = 1234567890  # Your telegram id (used for error reporting and private mode)
+BOT_TOKEN = "8470154984:AAGN-NX61M7IIyIYM71zqaABH-KZj6CTzLQ"  # Your telegram bot token
+DEVELOPER_ID = 6380679309  # Your telegram id (used for error reporting and private mode)
 IS_BOT_PRIVATE = False  # Change this to True to make bot private
